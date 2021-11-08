efae510b83c1687706dc6e190c3b6fd858fa9090
ooatamelbug <mohamedwork636@gmail.com>
2021-11-08 11:49:35 +0200

    add cors and config it and add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,36 @@
+// import express function from express plugin 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
+// make instance app from express function
 const app = express();
+
+// create variable type number to store the port in it
 const port: number = 3000;
 
+// create cors  options
+const optionCors: cors.CorsOptions = {
+    allowedHeaders:[
+        'Origin',
+        'Accept',
+        'Content-Type',
+        'X-Access-Token',
+        'X-Request-With'
+    ],
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS,HEAD',
+    origin: 'http://localhost:3000',
+    preflightContinue: false,
+};
+
+// middlewares 
+// use the cors and pass the options header
+app.use(cors(optionCors));
+// use the helmet 
+app.use(helmet());
+
+// app listen to the port and run application
 app.listen( port,() => {
     console.log(`app run in port ${port}`)
 });
